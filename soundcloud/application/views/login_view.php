8f0abbfcf6ba298767da315092d44ea5066ae7fb
iliade88 <cjg10@alu.ua.es>
2016-01-13 12:18:50 +0100

    Cambios en login

--- soundcloud/application/views/login_view.php
+++ soundcloud/application/views/login_view.php
@@ -2,21 +2,21 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	
 	<?php require_once('requires.php'); ?>
-	
 </head>
 <body>
 	<?php require_once('menu.php'); ?>
-<main>
-<form action="/soundcloud/index.php/login_controller/Login" method ="post">
-
-	Username: <input type="text" name="username"><br>
-	Password: <input type="password" name="pass"><br>
-	<input type="submit" value="Sign In"><br>
-
-</form>
-</main>
+	<main>
+		<form action="/soundcloud/index.php/login_controller/Login" method ="post">
+			<div class="form-group" style="min-width=480px; margin: auto; width: 50%;">
+				<label for="username">Username:</label>
+				<input type="text" class="form-control" name="username" placeholder="Username..." autofocus><br>
+				<label for="pass">Password:</label>
+				<input type="password" class="form-control" name="pass" palceholder="Password..."><br>
+				<input type="submit" class="btn btn-default" value="Sign In"><br>
+			</div>
+		</form>
+	</main>
 
 </body>
 </html>
